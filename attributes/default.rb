9c4368ee22eb6957c33ace335cebd98860008fdf
Alex <Alexandr.S.rus@gmail.com>
2013-10-13 20:31:08 +0400

    Updated versiong

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,7 +13,7 @@ default["httphq"]["userhome"] = "/home/#{node['httphq']['user']}"
 default["httphq"]["virtualenv"] = "/var/www/httphq"
 default["httphq"]["name"] = "httphq"
 
-default["httphq"]["version"] = "0.0.5"
+default["httphq"]["version"] = "0.0.6"
 
 default["httphq"]["servers"] = [{
   "host" => "127.0.0.1",
